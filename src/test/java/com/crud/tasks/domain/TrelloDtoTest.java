39bccc11178dec86559f56c472e5a7f8a6b974bb
trevize77 <maciej.spychalski@gmail.com>
2020-10-20 15:12:44 +0200

    Task29.4

--- src/test/java/com/crud/tasks/domain/TrelloDtoTest.java
+++ src/test/java/com/crud/tasks/domain/TrelloDtoTest.java
@@ -3,15 +3,13 @@ package com.crud.tasks.domain;
 
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+
 public class TrelloDtoTest {
 
 
