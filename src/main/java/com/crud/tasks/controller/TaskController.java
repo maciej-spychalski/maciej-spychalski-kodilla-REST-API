2008adb023bf01d280b8d84686ff0801ae055b3b
trevize77 <maciej.spychalski@gmail.com>
2020-11-09 10:50:35 +0100

    Task32.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,7 +12,6 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @CrossOrigin(origins = "*")
 @RestController
-//@RequestMapping("/v1/task")
 @RequestMapping("/v1")
 public class TaskController {
     @Autowired
@@ -21,39 +20,27 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
-//    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks")
-//    @GetMapping(value = "/tasks")
+    @GetMapping(value = "/tasks")
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
-//    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks/{taskId}")
-//    @GetMapping(value = "/tasks/{taskId}")
-//    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+    @GetMapping(value = "/tasks/{taskId}")
     public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
         return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
-//    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
-//    @DeleteMapping(value = "/tasks/{taskId}")
-//    public void deleteTask(@RequestParam Long taskId) {
+    @DeleteMapping(value = "/tasks/{taskId}")
     public void deleteTask(@PathVariable Long taskId) {
         service.deleteTask(taskId);
     }
 
-//    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-    @RequestMapping(method = RequestMethod.PUT, value = "/tasks")
-//    @PutMapping(value = "/tasks")
+    @PutMapping(value = "/tasks")
     public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
-//    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
-    @RequestMapping(method = RequestMethod.POST, value = "/tasks", consumes = APPLICATION_JSON_VALUE)
-//    @PostMapping(value = "/tasks", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/tasks", consumes = APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -18,14 +18,12 @@ public class TrelloController {
     @Autowired
     private TrelloFacade trelloFacade;
 
-//    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    @RequestMapping(method = RequestMethod.GET, value = "/boards")
+    @GetMapping(value = "/boards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloFacade.fetchTrelloBoards();
     }
 
-//    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
-    @RequestMapping(method = RequestMethod.POST, value = "/cards")
+    @PostMapping(value = "/cards")
     public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloFacade.createCard(trelloCardDto);
     }
